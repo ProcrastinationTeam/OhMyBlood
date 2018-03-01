a22137aa04ac5b2813faacaa00b9c903ee2096fa
LeRyokan <tixier.lucas@gmail.com>
2018-03-01 14:38:06 +0100

    Initial Commit

---
+++ source/Enemy.hx
@@ -0,0 +1,195 @@
+package;
+
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.addons.util.FlxFSM;
+import flixel.math.FlxMath;
+import flixel.math.FlxPoint;
+import flixel.math.FlxVelocity;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.tile.FlxTilemap;
+
+/**
+ * ...
+ * @author ElRyoGrande
+ */
+class Enemy extends FlxSprite 
+{
+	//FSM
+	public var fsm:FlxFSM<Enemy>;
+
+	public var _map:FlxTilemap;
+	
+	public var _player:Player;
+	
+	public var seePlayer:Bool = false;
+	
+	
+	//IA var
+	public var _distanceToPlayer:Int;
+	
+	public var checkWallRay:Bool;
+	
+	
+	
+	public function new(?X:Float=0, ?Y:Float=0, map:FlxTilemap, player:Player) 
+	{
+		super(X, Y);
+		_map = map;
+		_player = player;
+		this.loadGraphic("assets/images/enemy.png", true, 16, 16, false);
+		
+		this.setFacingFlip(FlxObject.RIGHT, false, false);
+		this.setFacingFlip(FlxObject.LEFT, true, false);
+		this.facing = FlxObject.RIGHT;
+		
+		this.animation.add("idle", [0]);
+		this.animation.add("walk", [0, 1], 6, true);
+		this.animation.play("idle");
+		
+		this.setSize(8, 16);
+		this.offset.set(4, 0);
+		acceleration.y = 500;
+		this.maxVelocity.set(150, 500);
+		
+		
+		fsm = new FlxFSM<Enemy>(this);
+		fsm.transitions
+		.add(EnemyIdle, Chase, EnemyConditions.see)
+		.add(Chase,EnemyIdle, EnemyConditions.idle)
+		.start(EnemyIdle);
+		
+		//RAYCAST SECTION 
+		checkWallRay = true;
+		
+	}
+	
+	override public function update(elapsed:Float):Void
+	{
+		
+		if (this.facing == FlxObject.RIGHT)
+		{
+			checkWallRay = this._map.ray(new FlxPoint(this.x,this.y+10), new FlxPoint(this.x + 10, this.y+10), 10);
+		}
+		else
+		{
+			checkWallRay = this._map.ray(new FlxPoint(this.x,this.y+10), new FlxPoint(this.x - 10, this.y+10), 10); 
+		}
+		
+		
+		trace("WALL RAY : " + checkWallRay);
+		
+		checkEnemyVision();
+		fsm.update(elapsed);
+		super.update(elapsed);
+	}
+	
+	override public function destroy():Void 
+	{
+		fsm.destroy();
+		fsm = null;
+		super.destroy();
+	}
+	
+	private function checkEnemyVision()
+	{
+		
+		//calcul de distance nécessaire
+		_distanceToPlayer = FlxMath.distanceBetween(this, _player);
+		trace("DISTANCE DU JOUEUR : " + _distanceToPlayer);
+		
+		if (_distanceToPlayer <= 100 &&  _map.ray(new FlxPoint(this.x,this.y), _player.getMidpoint()) && !_player.is_bathing )
+		{
+			seePlayer = true;
+		}
+		else
+		{
+			seePlayer = false;
+		}
+	}
+	
+}
+
+
+class EnemyConditions
+{
+	
+	public static function idle(Owner:Enemy)
+	{
+		return(!Owner.seePlayer);
+	}
+
+	
+	public static function see(Owner:Enemy)
+	{
+		return(Owner.seePlayer);
+	}
+	
+}
+
+class EnemyIdle extends FlxFSMState<Enemy>
+{
+	override public function enter(owner: Enemy, fsm:FlxFSM<Enemy>):Void
+	{
+		trace("ENEMY ENTER IDLE MODE");
+		owner.animation.play("idle");
+	}
+	
+	override public function update(elapsed:Float,owner: Enemy, fsm:FlxFSM<Enemy>):Void
+	{
+		//trace("ENNEMY IDLE");
+	}
+	
+	override public function exit(owner: Enemy):Void
+	{
+	}
+	
+}
+
+class Chase extends FlxFSMState<Enemy>
+{
+	override public function enter(owner: Enemy, fsm:FlxFSM<Enemy>):Void
+	{
+		trace("ENEMY ENTER CHASING MODE");
+		owner.animation.play("walk");
+	}
+	
+	override public function update(elapsed:Float,owner: Enemy, fsm:FlxFSM<Enemy>):Void
+	{
+		//trace("ENNEMY CHASE");
+		if (owner._distanceToPlayer <= 100  && owner._distanceToPlayer > 10)
+		{
+			//tentative de saut
+			if (!owner.checkWallRay && (owner.y - owner._player.y > 0))
+			{
+				owner.velocity.y =- 150; 
+			}
+			
+			
+			//aller vers le joueur
+			var dir = owner.x - owner._player.x;
+			if (dir < 0)
+			{
+				owner.velocity.x = 20;
+				owner.facing = FlxObject.RIGHT;
+			}
+			else
+			{
+				owner.velocity.x = -20;
+				owner.facing = FlxObject.LEFT;
+			}
+		}
+		else if (owner._distanceToPlayer <= 10)
+		{
+			//Attack
+			trace("Launch attack");
+			owner.velocity.x = 0;
+		}
+		
+	}
+	
+	override public function exit(owner: Enemy):Void
+	{
+		owner.velocity.x = 0;
+	}
+}
---
+++ source/PlayState.hx
@@ -0,0 +1,217 @@
+package;
+
+import flixel.FlxCamera;
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.input.keyboard.FlxKey;
+import flixel.math.FlxPoint;
+import flixel.text.FlxText;
+import flixel.tile.FlxTile;
+import flixel.tile.FlxTileblock;
+import flixel.tile.FlxTilemap;
+import flixel.util.FlxColor;
+using flixel.util.FlxSpriteUtil;
+using StringTools;
+
+class PlayState extends FlxState
+{
+	
+	private var _editMode : Bool;
+
+	private var _player : Player;
+	private var _enemy : Enemy;
+	
+	private var _map:FlxTilemap;
+	private var _mapTable:Array<FlxColor>;
+	private static inline var TILE_WIDTH:Int = 16;
+	private static inline var TILE_HEIGHT:Int = 16;
+	private var currentTileID:Int;
+	
+	
+	private var _info:String = "Current State: {STATE} \n";
+	private var _accX:String = "Acceleration X: {ACCX} \n";
+	private var _accY:String = "Acceleration Y: {ACCY} \n";
+	private var _velX:String = "Velocity X: {VELX} \n";
+	private var _velY:String = "Velocity Y: {VELY} \n";
+	private var _highRay:String = "High Ray : {THIGH} \n";
+	private var _lowRay:String = "Low Ray : {TLOW} \n";
+	private var _downLeftRay:String = "Down Left Ray: {DL} \n";
+	private var _downRightRay:String = "Down Right Ray: {DR} \n";
+	private var _upLeftRay:String = "Up Left Ray: {UL} \n";
+	private var _upRightRay:String = "Up Right Ray: {UR} \n";
+	private var _currentLeftTile:String = "Current Left Tile : {CurrentLeftTile} \n";
+	private var _currentRightTile:String = "Current Right Tile : {CurrentRightTile} \n";
+	
+	private var _txtInfo:FlxText;
+	private var _editModeTxt:FlxText;
+	
+	
+	//CAMERA SYSTEM
+	private var _camera : FlxCamera;
+	//private var _camera : FlxCameraFollowStyle;
+	
+	override public function create():Void
+	{
+		super.create();
+		bgColor = 0xff661166;
+		
+		
+		
+		var playerpos = new FlxPoint(0, 0);
+		
+		_editMode = false;
+		currentTileID = 1;
+		_mapTable = [FlxColor.WHITE, FlxColor.BLACK, FlxColor.BROWN, FlxColor.GRAY, FlxColor.RED];
+		_map = GenerateLevel("assets/data/fullMap.png", "assets/images/tiles.png", playerpos);
+		//_map = GenerateLevel("assets/data/fullMap.png", "assets/images/autotiles.png", playerpos);
+		add(_map);
+		
+		//INDISPENSABLE POUR QUE LE JEU CHARGE (FAIRE EN SORTE QU'IL SOIT PLUS GRAND QUE L'ENSEMBLE DES MAPS LOAD)
+		FlxG.worldBounds.set(0, 0, _map.width, _map.height);
+		
+		
+
+		_player = new Player(playerpos.x, playerpos.y, _map);
+		_enemy = new Enemy(playerpos.x + 150, playerpos.y, _map, _player); 
+		
+		//SIMPLE CAMERA A MODIFIER POUR LA RENDRE BIEN COOL
+		this.camera.follow(_player, SCREEN_BY_SCREEN, 0.2);
+		//this.camera.follow(_hero, SCREEN_BY_SCREEN, 0.2);
+		this.camera.snapToTarget();
+		
+		
+		add(_enemy);
+		add(_player);
+		add(_player.canvas);
+		add(_player.canvas2);
+		add(_player.canvas3);
+		add(_player.canvas4);
+		add(_player.canvas5);
+		add(_player.canvas6);
+		_txtInfo = new FlxText(16, 16, -1, _info);
+		_txtInfo.color = FlxColor.YELLOW;
+		add(_txtInfo);
+		
+		_editModeTxt = new FlxText(150, 20, -1, "EDIT MODE");
+		_editModeTxt.color = FlxColor.GREEN;
+		_editModeTxt.visible = false;
+		add(_editModeTxt);
+		
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		if (FlxG.mouse.pressed && _editMode)
+		{
+			//Rendre la map modifiable
+			_map.setTile(Std.int(FlxG.mouse.x / TILE_WIDTH), Std.int(FlxG.mouse.y / TILE_HEIGHT), currentTileID, true);
+			//map.setT
+			
+		}
+		
+		if (FlxG.keys.justPressed.PLUS)
+		{
+			changeCurrentTile();
+		}
+		
+		if (FlxG.keys.justPressed.SHIFT)
+		{
+			_editModeTxt.visible = !_editModeTxt.visible;
+			_editMode = !_editMode;
+		}
+		
+		
+		FlxG.collide(_player, _map);
+		FlxG.collide(_enemy, _map);
+		
+		//C'est degeu mais c'est que du debug
+		_editModeTxt.setPosition(this.camera.scroll.x + 150, this.camera.scroll.y + 20);
+		_txtInfo.setPosition( this.camera.scroll.x + 16, this.camera.scroll.y + 16);
+		
+		_txtInfo.text = _info.replace("{STATE}", Type.getClassName(_player.fsm.stateClass));
+		_txtInfo.text += _accX.replace("{ACCX}", Std.string(_player.acceleration.x));
+		_txtInfo.text += _accY.replace("{ACCY}", Std.string(_player.acceleration.y));
+		_txtInfo.text += _velX.replace("{VELX}", Std.string(_player.velocity.x));
+		_txtInfo.text += _velY.replace("{VELY}", Std.string(_player.velocity.y));
+		_txtInfo.text += _highRay.replace("{THIGH}", Std.string(_player.sideHighRay));
+		_txtInfo.text += _lowRay.replace("{TLOW}", Std.string(_player.sideLowRay));
+		_txtInfo.text += _downLeftRay.replace("{DL}", Std.string(_player.downLeftRay));
+		_txtInfo.text += _downRightRay.replace("{DR}", Std.string(_player.downRightRay));
+		_txtInfo.text += _upRightRay.replace("{UR}", Std.string(_player.upRightRay));
+		_txtInfo.text += _upLeftRay.replace("{UL}", Std.string(_player.upLeftRay));
+		_txtInfo.text += _currentLeftTile.replace("{CurrentLeftTile}", Std.string(_player.currentLeftTile));
+		_txtInfo.text += _currentRightTile.replace("{CurrentRightTile}", Std.string(_player.currentRightTile));
+		
+		if (FlxG.keys.anyJustPressed([FlxKey.R]))
+		{
+			FlxG.resetState();
+		}
+		
+		super.update(elapsed);
+	}
+	
+	
+	//ALGO GENERATION MAP
+	
+	public function GenerateLevel(imageMapPath:String, imageTilePath:String, playerPosR: FlxPoint):FlxTilemap
+	{ 
+		
+		//var mapTable = [FlxColor.WHITE, FlxColor.BLACK, FlxColor.BROWN, FlxColor.GRAY, FlxColor.RED];
+		var map = new FlxTilemap();
+		
+		//VERSION  WITHOUT AUTO TILES
+		map.loadMapFromGraphic(imageMapPath, false, 1, _mapTable, imageTilePath, 16, 16);
+		
+		//VERSION AVEC AUTO TILES
+		//map.loadMapFromGraphic(imageMapPath, false, 1, _mapTable, imageTilePath, 16, 16, AUTO);
+		
+		trace("LVL WIDTH : " + map.widthInTiles);
+		trace("LVL HEIGHT : " + map.heightInTiles);
+		
+		//Chargement de la position de départ du joueur
+		
+		var playerPos:Array<FlxPoint> = map.getTileCoords(_mapTable.length-1, false);
+		//player = new Hero(playerPos[0].x, playerPos[0].y);
+		
+		playerPosR.set(playerPos[0].x, playerPos[0].y);
+		
+		//Remove de la case propre au joueur
+		var playerTiles:Array<Int> = map.getTileInstances(_mapTable.length-1);
+		var playerTile:Int = playerTiles[0];
+		map.setTileByIndex(playerTile, 0, true);
+		
+		//var halfTile:Array<FlxPoint> = map.getTileCoords(mapTable.length - 2, false);
+		//var halfTile:Array<Int> = map.getTileInstances(mapTable.length - 2);
+		//trace("TILE VALUE : " + halfTile.length);
+		////
+		//var myTile:FlxTile = new FlxTile(map, 15, 16, 8, true, FlxObject.ANY);
+		//var myTileB:FlxTileblock = new FlxTileblock(0, 0, 16, 8);
+		//trace("MY TILE : " + myTile.index);
+		//
+		//for (i in halfTile)
+		//{
+			//
+		////	map.setTileByIndex(i,;
+		//}
+	//
+		//map.setTileProperties(mapTable.length - 2, FlxObject.ANY, halfTileCallback);
+	//	map.setTileByIndex(halfTile,
+		
+		return map;
+	}
+	
+	
+	public function changeCurrentTile()
+	{
+		currentTileID++;
+		
+		if (currentTileID > _mapTable.length - 1)
+		{
+			currentTileID = 0;
+		}
+		
+	}
+	
+}
